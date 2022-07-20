a4593f40cc9b233bd1876146bb09fe2d2c8a6a03
Norah <noura.ah1493@gmail.com>
2022-07-20 16:07:40 +0300

    first commit

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,47 +1,115 @@
 package calculator;
 
+import java.util.ArrayList;
+
 public class Calculator implements java.io.Serializable {
-	private double OperandOne;
-	private double OperandTwo;
-	private String Operation;
 	
-	public Calculator () {}
+	private ArrayList<Double> Operand = new ArrayList<Double>();
+	private ArrayList<String> Operation = new ArrayList<String>();
 	
+
+	public Calculator () {
+	}
 	
-	public double getOperandOne() {
-		return OperandOne;
+	public ArrayList<Double> getOperand() {
+		return Operand;
 	}
 
 
-	public void setOperandOne(double operandOne) {
-		OperandOne = operandOne;
+	public void setOperand(ArrayList<Double> Operand) {
+		Operand = Operand;
 	}
 
 
-	public double getOperandTwo() {
-		return OperandTwo;
-	}
-
-
-	public void setOperandTwo(double operandTwo) {
-		OperandTwo = operandTwo;
-	}
-
-
-	public String getOperation() {
+	public ArrayList<String> getOperation() {
 		return Operation;
 	}
 
 
-	public void setOperation(String operation) {
+	public void setOperation(ArrayList<String> operation) {
 		Operation = operation;
 	}
+	
 
-
-	public double performOperation() {
-		return (Operation=="+")? OperandOne+OperandTwo: OperandOne-OperandTwo;
+	// when passing an operand
+	public void performOperation(double operand) {
+		System.out.print(operand+" ");
+		Operand.add(operand);
 	}
-	public double getResults() {
-		return performOperation();
+	
+	// when passing an operation
+	public void performOperation(String operation) {
+		System.out.print(operation+" ");
+		Operation.add(operation);
+	}
+	
+	public boolean performOperation() {
+		
+		// when equal to 1, it supposed to have only '=' sign
+		while(Operation.size() > 1) {
+			
+			// Check Multiplication first 
+			if (Operation.contains("*")) {
+				
+				// get index of operation and remove it from list
+				int index =Operation.indexOf("*");
+				Operation.remove("*");
+				
+				//get and remove First Operand
+				double op1 = Operand.remove(index);
+				// get and remove Second Operand, used index because we removed first operand 
+				// so the second operand moved 
+				double op2 = Operand.remove(index);
+				
+				// add result to the list in the same index
+				Operand.add(index, op1*op2);
+				
+			}
+			else if (Operation.contains("/")) {
+				int index =Operation.indexOf("/");
+				Operation.remove("/");
+				
+				double op1 = Operand.remove(index);
+				double op2 = Operand.remove(index);
+				
+				
+				double result = op1/op2;
+				if (String.valueOf(result) == "Infinity") {
+					System.out.println("Divided by zero operation cannot possible");
+					return false;
+				}
+					
+				else
+					Operand.add(index, result);
+			}
+			
+			else if (Operation.contains("+")) {
+				
+				int index =Operation.indexOf("+");
+				Operation.remove("+");
+				
+				double op1 = Operand.remove(index);
+				double op2 = Operand.remove(index);
+				
+				Operand.add(index, op1+op2);
+				
+			}
+			else if (Operation.contains("-")) {
+				int index =Operation.indexOf("-");
+				Operation.remove("-");
+				
+				double op1 = Operand.remove(index);
+				double op2 = Operand.remove(index);
+				
+				Operand.add(index, op1-op2);
+				
+			}
+		}
+		return true;
+	}
+	
+	public Double getResults() {
+		
+		return performOperation()? Operand.get(0): null;
 	}
 }
--- src/calculator/TestCalculator.java
+++ src/calculator/TestCalculator.java
@@ -2,11 +2,49 @@ package calculator;
 
 public class TestCalculator {
 	public static void main(String [] args) {
+		System.out.println ("Operands priority: *,/,+,-");
 		Calculator cal1 = new Calculator();
-		cal1.setOperandOne(10.5);
-		cal1.setOperandTwo(5.2);
-		cal1.setOperation("+");
-		System.out.println(cal1.getResults());
+		cal1.performOperation(10.5);
+		cal1.performOperation("+");
+		cal1.performOperation(5.2);
+		cal1.performOperation("*");
+		cal1.performOperation(10);
+		cal1.performOperation("=");
+
+		Double result = cal1.getResults();
+		if (result != null) {
+			System.out.println(result);
+		}
+		
+		
+		Calculator cal2 = new Calculator();
+		cal2.performOperation(10);
+		cal2.performOperation("-");
+		cal2.performOperation(25);
+		cal2.performOperation("/");
+		cal2.performOperation(5);
+		cal2.performOperation("*");
+		cal2.performOperation(5);
+		cal2.performOperation("=");
+		result = cal2.getResults();
+		if (result != null) {
+			System.out.println(result);
+		}
+		
+		Calculator cal3 = new Calculator();
+		cal3.performOperation(10);
+		cal3.performOperation("-");
+		cal3.performOperation(25);
+		cal3.performOperation("/");
+		cal3.performOperation(5);
+		cal3.performOperation("*");
+		cal3.performOperation(0);
+		cal3.performOperation("=");
+		result = cal3.getResults();
+		if (result != null) {
+			System.out.println(result);
+		}
+		
 		
 	}
 }
